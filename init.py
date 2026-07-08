0676d72f5c96545b9a640d108454e9734656caa6
ggnicolas28 <ggnicolas28@gmail.com>
2026-07-08 18:00:19 -0300

    V 2.0

--- init.py
+++ init.py
@@ -8,8 +8,25 @@ print(df)
 print("\n")
 
 
-print("Peças > 50\n")
-print(df[df['Peças boas'] > 50])
+
+#Criando uma nova coluna com a porcentagem de peças boas
+df["Boas (%)"] = df['Peças boas'] / df['Total Previsto'] * 100
+print(df)
+print("\n")
+
+#Mudando o total previsto
+df['Total Previsto'] = 200
+
+#Criando uma nova coluna com a porcentagem de peças ruins
+df["Ruins (%)"] = df['Com defeito'] / df['Total Previsto'] * 100
+print(df)
+print("\n")
+
+#Imprimindo máquinas com melhor desempenho
+maior_que_50 = df['Peças boas'] > 50
+
+print("Maquinas com bom aproveitamento (mais de 50 peças boas)\n")
+print(df[maior_que_50])
 print("\n")
 
 print("Descrição da tabela:\n")
